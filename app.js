4170693cf648c62b8ae1b5439c26e9f7c5eb0db3
Stuart Alexander <stuartalexander2@gmail.com>
2020-07-03 10:27:35 +0100

    Final Submission

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const overlay = document.getElementById("overlay");
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
-let startGame = document.querySelector(".btn__reset");
+const phraseListParent = document.querySelector("ul");
+const startGame = document.querySelector(".btn__reset");
 let title = document.querySelector(".title");
-
-
-
 let missed = 0;
 const phrases = ["Beating Around the Bush", "Curiosity Killed The Cat", "Right Off the Bat","Wake Up Call","Barking Up The Wrong Tree"];
 
@@ -15,18 +13,16 @@ const phrases = ["Beating Around the Bush", "Curiosity Killed The Cat", "Right O
 //Function to get a random Phrase
 
 function getRandomPhrase (arr){
-  const randomNumber = Math.floor( Math.random() * arr.length);
-  const getPhrase = arr[randomNumber].toString().toLowerCase();
-  for (let i = 0; i < getPhrase.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = getPhrase.charAt([i]);
-
-  if (li.textContent !== " "){
+  const randomNumber = Math.floor( Math.random() * arr.length); //creates a random number between 0 and the length of the array passed
+  const getPhrase = arr[randomNumber].toString().toLowerCase(); //selects the phrase from the array using the random number returned
+  for (let i = 0; i < getPhrase.length; i++) { //loop to run through each letter in phrase
+  const li = document.createElement("li"); //create new list item for each letter from the phrase
+  li.textContent = getPhrase.charAt([i]); //inputs each letter using the index into the newly created list item
+  if (li.textContent !== " "){ 
     li.classList.add("letter");
   } else
     li.classList.add("space");
-  let ul = document.querySelector("ul");
-  ul.appendChild(li);
+    phraseListParent.appendChild(li); //appends the new elements into the ul parent
   }
   return getPhrase;
 }
@@ -73,29 +69,27 @@ function checkWin (){
     }   
     }
 
+//function to reset the game.
+
   function reset (){
     const button = document.getElementsByTagName("button");
     const hearts = document.getElementsByClassName("tries")
-    const phrase = document.getElementsByClassName("show");
+    const phraseParent = document.querySelector("ul").children;
     overlay.style.display = "none";
     missed = 0;
     for ( let i = 0; i < button.length; i++ ){
       button[i].className = "";
       button[i].disabled = false;
     for (let i = 0; i < hearts.length; i++){
-      hearts[i].innerHTML = "<img src = 'images/liveheart.png' height='35px' width = '30px'>";
+      hearts[i].innerHTML = "<img src = 'images/liveheart.png' height='35px' width = '30px'>"; //reverts image back to blue heart
     }
-    for ( let i = 0; i < phrase.length; i++ ){
-      phrase[i].classList.remove("show");
-      
-    }
-    }
-    
-    }
+    for ( let i = 0; i < phraseParent.length; i++ ){
+      phraseParent[i].remove(); //removes all of the list items
 
-  
-
-
+    }
+    }
+ getRandomPhrase(phrases)
+    }
             
 //Hide the start screen overlay  
 
@@ -107,29 +101,28 @@ startGame.addEventListener("click", () => {
 }); 
 
 
-    //Event lister for keyoard
+//Event lister for keyoard
 
 qwerty.addEventListener("click", (event) => {
   if (event.target.tagName == "BUTTON"){
     const button = event.target;
     const letterClicked = event.target.innerHTML;
-    const ol = document.querySelector("ol");
-    button.classList.add("chosen");
-    button.disabled = "true";
     let letterFound = checkLetter(letterClicked);
+    button.classList.add("chosen");
+    button.disabled = "true"; 
     if (letterFound == null){   
       missed += 1; 
-      let li = document.getElementsByClassName("tries")[0];      
-      if (li.parentNode) {
-        li.parentNode.removeChild(li);
-      }
-      let lostHeart = document.createElement("li");
+      let li = document.querySelectorAll(".tries")[0];      
+      let hearts = document.querySelectorAll("ol")[0].children;   //select all of the heart elements    
+      hearts[4].remove();      //remove the last heart 
+      let lostHeart = document.createElement("li");//create an element for lost hearts
       lostHeart.innerHTML = "<img src = 'images/lostheart.png' height='35px' width = '30px'>";
-      lostHeart.classList.add("tries");  
-      ol.insertBefore(lostHeart, li.nextElementSibling)
-
+      lostHeart.classList.add("tries");      
+      let heartContainer = document.querySelectorAll("ol")[0];//get list of ol list child elements
+      heartContainer.insertBefore(lostHeart, hearts[0]) //inserst new lostHeart element as first child.
      
 }}
 checkWin();
 });
+
 
